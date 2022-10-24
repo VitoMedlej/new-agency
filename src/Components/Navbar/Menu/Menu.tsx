a1f4b1b9c1fa2817880e91c591b09a0dcb2b4b6a
Vito Medlej <rip.vitto@gmail.com>
2022-10-24 12:34:29 +0300

    menu changes

--- src/Components/Navbar/Menu/Menu.tsx
+++ src/Components/Navbar/Menu/Menu.tsx
@@ -24,77 +24,106 @@ const Menu = () => {
                     xs: 0,
                     md: '33%',
                     xl: '25%'
-                }
+                },
+                
             }}>
-                hello world
+                <Box sx={{
+                    height: '50%'
+                }}></Box>
+                <Box
+                className='center'
+                sx={{
+                    px:'1em',
+                    flexDirection: 'column',
+                    background:'#6000a9',
+                    height: '50%'
+                }}>
+                    <Typography sx={{fontSize:'2em',letterSpacing:'.54em',fontWeight:'bolder',color:'white'}}>
+                        FOLLOW US
+                    </Typography>
+                    <SocialMedia
+                        sx={{
+                        display: {
+                            md: 'none'
+                        }
+                    }}/>
+                </Box>
+
             </Box>
             <Box
-            
                 sx={{
-            
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'baseline',
-    justifyContent: 'center',
-                    background: '#19181d !important',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'baseline',
+                justifyContent: 'center',
+                background: '#19181d !important',
                 height: '100%',
                 width: {
                     xs: '100%',
                     md: '67%',
                     xl: '75%'
                 },
-                pl:'1.5em'
-                // alignItems: {
-                //     xs: 'baseline',
-                //     md: 'center'
-                // },
-                // justifyContent: 'space-between'
+                pl: '1.5em'
             }}>
-             
-                <Box className='menu-links center' sx={{pt:{xs:'2em',sm:0}}}>
-                    <Link href='/'>
-                    <Typography>
-                        Home
-                    </Typography>
-                    </Link>
-                    <Link href='/'>
-                    <Typography>
-                        Home
-                    </Typography>
-                    </Link>
-                    <Link href='/'>
-                    <Typography>
-                        Home
-                    </Typography>
-                    </Link>
-                    <Link href='/'>
-                    <Typography>
-                        Home
-                    </Typography>
-                    </Link>
-                 
+
+                <Box
+                    className='menu-links '
+                    sx={{
+                    pt: {
+                        xs: '2em',
+                        sm: 0
+                    }
+                }}>
+
+                    {[
+                        {
+                            title: 'HOME',
+                            href: '/'
+                        }, {
+                            title: 'SERVICES',
+                            href: '/services'
+                        }, {
+                            title: 'PORTFOLIO',
+                            href: '/portfolio'
+                        }, {
+                            title: 'ABOUT',
+                            href: '/about'
+                        }, {
+                            title: 'PRICING',
+                            href: '/pricing'
+                        }
+                    ].map(link => {
+                        return <Link href={`${link.href}`}>
+                            <Typography>
+                                {link.title}
+                            </Typography>
+                        </Link>
+                    })
+}
 
                 </Box>
-                <SocialMedia sx={{display:{md:'none'}}}/>
             </Box>
-                <IconButton
-                    className='cursor close-btn'
+            <IconButton
+                className='cursor close-btn'
+                sx={{
+                position: 'absolute',
+                top: '0%',
+                right: '0%',
+                ':hover': {
+                    color: 'red'
+                },
+                margin: '1em'
+            }}>
+                <CancelIcon
+                    fontSize='large'
                     sx={{
-                        position:'absolute',
-                        top:'0%',
-                        right:'0%',
-                        ':hover' : {color:'red'},
-
-                    margin: '1em'
-                }}>
-                    <CancelIcon
-                        fontSize='large'
-                        sx={{
-                        ':hover' : {color:'red'},
-                        color: 'white',
-                        transform: 'scale(1.3)'
-                    }}/>
-                </IconButton>
+                    ':hover': {
+                        color: 'red'
+                    },
+                    color: 'white',
+                    transform: 'scale(1.3)'
+                }}/>
+            </IconButton>
         </Box>
     )
 }
--- src/Components/SocialMedia/SocialMedia.tsx
+++ src/Components/SocialMedia/SocialMedia.tsx
@@ -1,14 +1,27 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
-import InstagramOutlinedIcon from '@mui/icons-material/InstagramOutlinedIcon'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 const SocialMedia = (sx:any) => {
   return (
-    <Box sx={{...sx}}>
-        <a href='/' className='social-link'>
-        <InstagramOutlinedIcon/>
+    <Box  sx={{...sx,display:'flex',gap:'1em',mt:'1em'}}>
+        <a href='/' className=' ins'>
+        <InstagramIcon fontSize='large'/>
         </a>
+        <a href='/' target='_blank' className=' fb'>
+        <FacebookOutlinedIcon fontSize='large'/>
+        </a>
+        <a href='/' target='_blank' className=' wts'>
+        <WhatsAppIcon fontSize='large'/>
+        </a>
+
+        <a href='/' target='_blank' className=' phn'>
+        <LocalPhoneIcon fontSize='large'/>
+        </a>
+        
     </Box>
   )
 }
