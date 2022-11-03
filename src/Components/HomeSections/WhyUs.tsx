4cf8330129b992325321016048be35f563d8eed8
Vito Medlej <rip.vitto@gmail.com>
2022-11-03 14:18:14 +0200

    Section edits

--- src/Components/HomeSections/WhyUs.tsx
+++ src/Components/HomeSections/WhyUs.tsx
@@ -1,38 +1,66 @@
-import { Box, Typography } from '@mui/material'
-import { useRouter } from 'next/router';
+import {Box, Typography} from '@mui/material'
+import {useRouter} from 'next/router';
 import React from 'react'
-import { lang } from '../Navbar/Navbar';
+import {lang} from '../Navbar/Navbar';
 
 const WhyUs = () => {
     const router = useRouter()
     const {l} = router.query;
-  return (
-    <Box
-    sx={{
-    my: '5em',
-    px: '1em',
-    py:'2em',
-    position: 'relative',
-    background: '#6000a9',
-    color:'white',
-    textAlign: 'center',
-}}>
-    <Box
-        sx={{
-    }}
-        >
-        <Typography className='h1'>
-            {lang('Why Choose Us?',"لما تخترنا؟",l)}
-        </Typography>
-        <Typography sx={{color:'#c2c2c2'}}>
-        {lang('this is some random ass text, dw about it nigga',"ما تخترن تخترن تخترن تخترن تخترن تخترن",l)}
-        </Typography>
-    </Box>
-    <Box sx={{background:'gray',width:{md:'80%'}}}>
-    hello wolrd
-    </Box>
-</Box>
-  )
+    return (
+        <Box
+            sx={{
+            my: '5em',
+            px: '1em',
+            position: 'relative',
+            background: '#6000a9',
+            color: 'white',
+            textAlign: 'center'
+        }}>
+            <Box
+                sx={{
+                pt: '1.5em',
+                px: '1em'
+            }}>
+                <Typography className='h1'>
+                    {lang('Why Choose Us?', "لما تخترنا؟", l)}
+                </Typography>
+                <Typography sx={{
+                    color: '#c2c2c2'
+                }}>
+                    {lang('this is some random ass text, dw about it nigga', "ما تخترن تخترن تخترن تخترن تخترن تخترن", l)}
+                </Typography>
+            </Box>
+            <Box
+            className='mw'
+                sx={{
+                margin: '0 auto',
+                transform: {sm:'translateY(25%)',md:'translateY(50%)'},
+                background: {md:'gray'},
+                width: {
+                    md: '90%'
+                },
+                padding:'1em',
+                gap:'1em',
+                borderRadius: '9px',
+                flexWrap: 'wrap',
+                display:'flex',justifyContent: 'space-around'
+            }}>
+                <Box sx={{width:{xs:'47%',md:'21%'},height:'100px',background:'red'}}>
+                        Lorem ipsum dolor sit amet.
+                </Box>
+                <Box sx={{width:{xs:'47%',md:'21%'},height:'100px',background:'red'}}>
+                        Lorem ipsum dolor sit amet.
+                </Box>
+                <Box sx={{width:{xs:'47%',md:'21%'},height:'100px',background:'red'}}>
+                        Lorem ipsum dolor sit amet.
+                </Box>
+                <Box sx={{width:{xs:'47%',md:'21%',},height:'100px',background:'red'}}>
+                        Lorem ipsum dolor sit amet.
+                </Box>
+              
+            </Box>
+        </Box>
+    )
 }
 
 export default WhyUs
