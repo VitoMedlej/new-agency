95f8c13abac7315f455da59a9b02eb7cd0dfe2ab
vito medlej <vito.medlej@gmail.com>
2022-11-04 10:17:22 +0300

    whyus section changes

--- src/Components/HomeSections/WhyUs.tsx
+++ src/Components/HomeSections/WhyUs.tsx
@@ -3,6 +3,52 @@ import {useRouter} from 'next/router';
 import React from 'react'
 import {lang} from '../Navbar/Navbar';
 
+const Reasons = [
+    {
+        title: {
+            en: 'The Cheapest',
+            ar: 'الارخص'
+        },
+        text: {
+            en: 'lorem the fuckeig fuocok fasf',
+            ar: 'بشسب شسبش لضفتخ -ضصنل شجسصن لج جيح'
+        }
+    ,    img:'https://softweb.co.in/wp-content/uploads/2022/10/pace-new.png',
+
+    }, {
+        title: {
+            en: 'The Cheapest',
+            ar: 'الارخص'
+        },
+        text: {
+            en: 'lorem the fuckeig fuocok fasf',
+            ar: 'بشسب شسبش لضفتخ -ضصنل شجسصن لج جيح'
+        }
+    ,    img:'https://softweb.co.in/wp-content/uploads/2022/10/pace-new.png',
+    
+    }, {
+        title: {
+            en: 'The Cheapest',
+            ar: 'الارخص'
+        },
+        text: {
+            en: 'lorem the fuckeig fuocok fasf',
+            ar: 'بشسب شسبش لضفتخ -ضصنل شجسصن لج جيح'
+        },
+        img:'https://softweb.co.in/wp-content/uploads/2022/10/pace-new.png',
+
+    }, {
+        img:'https://softweb.co.in/wp-content/uploads/2022/10/pace-new.png',
+        title: {
+            en: 'The Cheapest',
+            ar: 'الارخص'
+        },
+        text: {
+            en: 'lorem the fuckeig fuocok fasf',
+            ar: 'بشسب شسبش لضفتخ -ضصنل شجسصن لج جيح'
+        }
+    }
+]
 const WhyUs = () => {
     const router = useRouter()
     const {l} = router.query;
@@ -31,33 +77,66 @@ const WhyUs = () => {
                 </Typography>
             </Box>
             <Box
-            className='mw'
+                className='mw'
                 sx={{
                 margin: '0 auto',
-                transform: {sm:'translateY(25%)',md:'translateY(50%)'},
-                background: {md:'gray'},
+                transform: {
+                    xs: 'translateY(10%)',
+                    sm: 'translateY(25%)',
+                    md: 'translateY(35%)'
+                },
                 width: {
                     md: '90%'
                 },
-                padding:'1em',
-                gap:'1em',
-                borderRadius: '9px',
+                padding: '1em',
+                gap: '1em',
                 flexWrap: 'wrap',
-                display:'flex',justifyContent: 'space-around'
-            }}>
-                <Box sx={{width:{xs:'47%',md:'21%'},height:'100px',background:'red'}}>
-                        Lorem ipsum dolor sit amet.
-                </Box>
-                <Box sx={{width:{xs:'47%',md:'21%'},height:'100px',background:'red'}}>
-                        Lorem ipsum dolor sit amet.
-                </Box>
-                <Box sx={{width:{xs:'47%',md:'21%'},height:'100px',background:'red'}}>
-                        Lorem ipsum dolor sit amet.
-                </Box>
-                <Box sx={{width:{xs:'47%',md:'21%',},height:'100px',background:'red'}}>
-                        Lorem ipsum dolor sit amet.
-                </Box>
-              
+                color: 'black',
+                boxShadow: '1px 1px 3px #00000036',
+                display: 'flex',
+                background: 'white',
+                justifyContent: 'space-around',
+                borderRadius:"9px",
+}}>
+
+                {Reasons.map((reason, idx : number) => {
+
+                    return <Box
+                        key={idx}
+                        sx={{
+                        width: {
+                            xs: '42%',
+                            sm : '25%',
+                            md: '21%'
+                        },
+                        padding: '.75em',
+                        height: '100px',
+                        background: 'white',
+                        position: 'relative',
+                        color:'white',
+                        zIndex: '111',
+                        backgroundImage: `url('${reason?.img}')`
+                    }}>
+                        <Box
+                            className='absolute'
+                            sx={{
+                            background: '#00000070',
+                            zIndex: '-1'
+                        }}></Box>
+                        <Typography
+                            sx={{
+                            fontSize: '1.5em',
+                            fontWeight: '700'
+                        }}>
+                            {lang(reason.title.en, reason.title.ar, l)}
+                        </Typography>
+                        <Typography >
+                            {lang(reason.text.en, reason.text.ar, l)}
+
+                        </Typography>
+                    </Box>
+                })}
+
             </Box>
         </Box>
     )
