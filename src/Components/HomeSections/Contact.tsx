f4ce4cd09551d1f3a11cc6bd8ad34022dac7e71e
Vito Medlej <rip.vitto@gmail.com>
2022-11-05 10:14:29 +0200

    adding contact section

--- src/Components/HomeSections/Contact.tsx
+++ src/Components/HomeSections/Contact.tsx
@@ -1,7 +1,12 @@
-import {Box} from '@mui/material'
+import {Box, Button, TextField} from '@mui/material'
+import {useRouter} from 'next/router'
 import React from 'react'
+import {lang} from '../Navbar/Navbar'
+import Typo from '../Typography/Typo'
 
 const Contact = () => {
+    const router = useRouter();
+    const {l} = router.query;
     return (
         <Box
             sx={{
@@ -9,20 +14,68 @@ const Contact = () => {
             mx: '1em',
             mb: '5em',
             pt: '2em',
-            position: 'relative',
-            display:'none',
+            position: 'relative'
         }}>
             <Box
                 sx={{
-                padding: '1em',
-                width: '95%',
-                background: '#3D4A6D',
-                borderRadius: '9px',
-                color:'white'
+                display: 'flex',
+                justifyContent: {xs:'center',md:'space-between'}
             }}
                 className='mw'>
-                Text
+
+                <Box sx={{
+                    width: '57%',
+                    display : {xs:'none',md:'block'}
+                }}>
+                    <img
+                        className='img'
+                        src="https://images.pexels.com/photos/7792770/pexels-photo-7792770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        alt=""/>
+                </Box>
+                <Box
+                    sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'end',
+                    padding: '1em',
+                    width: {xs:'100%',md:'43%'},
+                    maxWidth: '500px',
+                    borderRadius: '9px',
+                    textAlign: lang('left', 'right', l)
+                }}>
+                    <Typo className='h1' en='Contact Us' ar='تواصل معنا'/>
+                    <Typo
+                        sx={{
+                        color: '#000000b8',
+                        pt: '.25em'
+                    }}
+                        en='contact us now bitch or else imma kill you'
+                        ar='  تواصل معنا الان والا قتلتك يا بيتش هاها'/>
+                    <Box
+                        component='form'
+                        sx={{
+                        display: 'flex',
+                        mt: '2em',
+                        flexDirection: 'column',
+                        gap : '1.25em',
+                        width: '90%',
+                        justifyContent: 'right'
+                    }}>
+                        <TextField id="outlined-basic" label="الاسم" variant="outlined"/>
+                        <TextField id="outlined-basic" label="Outlined" variant="outlined"/>
+                        <TextField id="outlined-basic" label="Outlined" variant="outlined"/>
+                        <TextField id="outlined-basic" label="Outlined" variant="outlined"/>
+                        <Button className='btn b2'>
+                            {lang('OUR WORK', ' مشاريعنا', `${l}`)}
+                        </Button>
+
+                    </Box>
+                </Box>
             </Box>
+                <Box className='mw'>
+                    faksfasf
+                </Box>
+
         </Box>
     )
 }
