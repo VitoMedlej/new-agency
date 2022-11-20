3dcbe1f98fa4bbbb711686959b69cddea3f93c98
Vito Medlej <rip.vitto@gmail.com>
2022-11-20 12:09:06 +0200

    removed unused imports

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Layout from '../src/Layout/Layout'
-import {Box, Button, Typography} from '@mui/material'
-import {useRouter} from 'next/router';
-import {lang} from '../src/Components/Navbar/Navbar';
-import Typo from '../src/Components/Typography/Typo';
+
 import HeadSection from '../src/Components/PricingSections/HeadSection';
 import OurPricing from '../src/Components/PricingSections/OurPricing';
 
